023402d1f79bdcfc9bae70c97b4867d2662dccef
Wolfpleasa <dominhngoc2611@gmail.com>
2021-04-11 09:28:04 +0700

    fix

--- jsHomework/day3/index.js
+++ jsHomework/day3/index.js
@@ -143,3 +143,123 @@
 //     })
 // }
 // console.log(findDo(students));
+
+//bai29
+// var students = [{
+//         id: "T3HXX1",
+//         firstName: "NgAN",
+//         lastName: "Duong Thuy"
+//     },
+//     {
+//         id: "T3HXX2",
+//         firstName: "Ha",
+//         lastName: "Do Thi Thu"
+//     },
+//     {
+//         id: "T3HXX5",
+//         firstName: "Minh",
+//         lastName: "Nguyen Nhat"
+//     }
+// ]
+
+// function chuanHoa(str) {
+//     var str1 = str.toLowerCase().split(" ");
+//     var str2 = str1.map(function(val) {
+//         return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+//     });
+//     return str2.join(" ");
+// }
+
+// function sortName(students) {
+//     for (var student of students) {
+//         student.firstName = chuanHoa(student.firstName);
+//         student.lastName = chuanHoa(student.lastName);
+//     }
+//     return students.sort((a, b) => a.firstName.localeCompare(b.firstName));
+// }
+
+// console.log(sortName(students));
+//bai30
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+// var OK = 0;
+// function secondMax(arr) {
+//     var arr1 = arr.sort();
+//     var max = arr1[arr1.length - 1];
+//     for (var i = arr1.length - 2; i >= 0; i--) {
+//         if (max > arr1[i]) {
+//             max = arr1[i];
+//             OK = 1;
+//             break;
+//         }
+//     }
+//     return max;
+// }
+
+//if(OK===1) console.log(secondMax(arr));
+//else console.log(-1);
+
+//bai31
+// arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+// var k = 27;
+// var OK = 0;
+
+// function quayLui(i) {
+//     if (i > 2) return false;
+//     for (var j = i; j < arr.length; j++) {
+//         k -= arr[j];
+//         if (k !== 0 || i !== 2) quayLui(i + 1);
+//         else OK = 1;
+//         k += arr[j];
+//     }
+// }
+
+// quayLui(0);
+// if (OK === 1) console.log("YES");
+// else console.log("NO");
+
+// function tinh(arr) {
+//     arr.sort();
+//     for (var i = 0; i < arr.length; i++) {
+//         var j = i + 1;
+//         var n = arr.length - 1
+//         if (arr[i] + arr[j] + arr[n] === k) return true;
+//         (arr[i] + arr[j] + arr[n] - k > 0) ? j++ : n--;
+//     }
+//     return false;
+// }
+// if (tinh(arr)) console.log("YES");
+// else console.log("NO");
+
+//bai32
+// var arr = [1, 3, 6, 9, 11, 20];
+// var k = 13;
+
+// function insertK(arr) {
+//     for (var i = 0; i <= arr.length - 2; i++) {
+//         if (arr[i] <= k && k <= arr[i + 1]){
+//             arr.splice(i+1, 0, k);
+//             break;
+//         }
+//     }
+//     console.log(arr);
+// }
+
+// insertK(arr);
+
+//bai33
+// var arr = [9, 8, 7, 6, 4, 5, 3, 2, 1];
+
+// function bubbleSort(arr) {
+//     for (var i = 0; i < arr.length - 1; i++) {
+//         for (var j = arr.length - 1; j > i; j--) {
+//             if (arr[i] > arr[j]) {
+//                 var t = arr[i];
+//                 arr[i] = arr[j];
+//                 arr[j] = t;
+//             }
+//         }
+//     }
+//     return arr;
+// }
+
+// console.log(bubbleSort(arr));
